7ed9b65f7ffb77c6a0c5c540d55e044bc4c73259
Jaume Sanchez <the.spite@gmail.com>
2016-09-26 15:47:44 +0200

    multiline support (might be rolled back)

--- graph/graph.js
+++ graph/graph.js
@@ -3,26 +3,26 @@
 	"use strict";
 
 	function throttle(fn, threshold, scope) {
-	threshold || (threshold = 250);
-	var last,
-	deferTimer;
-	return function () {
-		var context = scope || this;
+		threshold || (threshold = 250);
+		var last,
+		deferTimer;
+		return function () {
+			var context = scope || this;
 
-		var now = +new Date,
-		args = arguments;
-		if (last && now < last + threshold) {
-			clearTimeout(deferTimer);
-			deferTimer = setTimeout(function () {
+			var now = +new Date,
+			args = arguments;
+			if (last && now < last + threshold) {
+				clearTimeout(deferTimer);
+				deferTimer = setTimeout(function () {
+					last = now;
+					fn.apply(context, args);
+				}, threshold);
+			} else {
 				last = now;
 				fn.apply(context, args);
-			}, threshold);
-		} else {
-			last = now;
-			fn.apply(context, args);
-		}
-	};
-}
+			}
+		};
+	}
 
 	function debounce(fn, delay) {
 		var timer = null;
@@ -35,14 +35,89 @@
 		};
 	}
 
+	function colorLuminance(hex, lum) {
+
+		// validate hex string
+		hex = String(hex).replace(/[^0-9a-f]/gi, '');
+		if (hex.length < 6) {
+			hex = hex[0]+hex[0]+hex[1]+hex[1]+hex[2]+hex[2];
+		}
+		lum = lum || 0;
+
+		// convert to decimal and change luminosity
+		var rgb = "#", c, i;
+		for (i = 0; i < 3; i++) {
+			c = parseInt(hex.substr(i*2,2), 16);
+			c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
+			rgb += ("00"+c).substr(c.length);
+		}
+
+		return rgb;
+	}
+
+	function Line( color ) {
+
+		this.data = [];
+		this.linePath = new Path2D();
+		this.solidPath = new Path2D();
+
+		this.color = color;
+		this.secondaryColor = colorLuminance( color, -.25  )
+
+	}
+
+	Line.prototype.set = function( data ) {
+
+		this.data = data;
+		this.updatePath();
+
+	}
+
+	Line.prototype.updatePath = function() {
+
+		this.linePath = new Path2D();
+
+		var ovx = 0;
+		var ovy = 0;
+		var start = this.data[ 0 ].x;
+
+		this.data.forEach( ( v, i ) => {
+
+			var vx = v.x - start;
+			var vy = v.y;
+
+			if( i === 0 ) {
+				this.linePath.moveTo( vx, vy );
+				ovx = vx;
+				ovy = vy;
+			} else {
+				var cpx = ovx + ( vx - ovx ) * .5;
+				var cpy1 = ( vy < ovy ) ? vy : ovy;
+				var cpy2 = ( vy < ovy ) ? ovy : vy;
+				this.linePath.bezierCurveTo( cpx, cpy1, cpx, cpy2, vx, vy );
+				ovx = vx;
+				ovy = vy;
+			}
+
+		} );
+
+		this.solidPath = new Path2D( this.linePath );
+
+		this.solidPath.lineTo( this.data[ this.data.length - 1 ].x - start, 0 );
+		this.solidPath.lineTo( 0, 0 );
+
+	}
+
+
 	function Graph( properties ) {
+
+		this.lines = [];
 
 		this.properties = properties;
 
 		this.properties.baselines = this.properties.baselines || [];
 		this.decorator = this.properties.decorator || ( v => v );
 
-		this.data = [];
 		this.start = 0;
 		this.end = 0;
 		this.paddingTop = 2;
@@ -119,7 +194,7 @@
 
 		this.canvas.addEventListener( 'wheel', e => {
 
-			debouncedLinkZoom( this.zoom + ( .005 * e.deltaY ) );
+			debouncedLinkZoom( this.zoom - ( .05 * e.deltaY ) );
 			e.preventDefault();
 
 		} );
@@ -142,7 +217,8 @@
 	Graph.prototype.updateZoom = function( zoom ) {
 
 		this.zoom = zoom;
-		if( this.zoom > 1 ) this.zoom = 1;
+		if( this.zoom < 1 ) this.zoom = 1;
+		this.zoom = 1;
 		this.update();
 
 	}
@@ -160,17 +236,20 @@
 
 	Graph.prototype.updatePoint = function( x ) {
 
+		return;
 		if( x === undefined ) x = this.lastPoint;
 		this.lastPoint = x;
 
-		if( this.data.length === 0 ) return;
+		if( this.line.data.length === 0 ) return;
 
 		var res = this.updateLabelPosition( x );
-		var pos = res.x * ( this.end - this.start ) / res.width + this.start;
+		var pos = res.x * ( this.last - this.first ) / res.width + this.first;
 		this.pivot = res.x / res.width;
-		var y = ( this.data.find( v => v.x >= pos ) ).y;
-		this.label.textContent = this.decorator( y );
-		y = ( this.canvas.clientHeight - this.paddingTop ) * this.dpr * ( 1 - ( y / this.max ) ) + this.paddingTop * this.dpr;
+		this.lines.forEach( line => {
+			var y = ( line.data.find( v => v.x >= pos ) ).y;
+			this.label.textContent = this.decorator( y );
+			y = ( this.canvas.clientHeight - this.paddingTop ) * this.dpr * ( 1 - ( y / this.max ) ) + this.paddingTop * this.dpr;
+		} );
 
 		var x = res.x * this.dpr;
 
@@ -190,6 +269,8 @@
 		this.overlayCtx.moveTo( x, this.overlayCanvas.height );
 		this.overlayCtx.lineTo( x, y + 3 );
 		this.overlayCtx.stroke();
+
+		this.invalidate = true;
 
 	}
 
@@ -236,29 +317,40 @@
 
 	Graph.prototype.set = function( data ) {
 
-		this.data = data;
+		//this.lines = new Line( properties.color );
+		//this.line.set( data );
+		if( !Array.isArray( data ) ) data = [ data ];
+		data.forEach( d => {
+			var line = new Line( d.color );
+			line.set( d.samples );
+			this.lines.push( line );
+		} );
+
 		this.update();
-		this.updatePath();
 
 	}
 
 	Graph.prototype.update = function() {
 
-		if( this.data.length === 0 ) return;
+		if( this.lines[ 0 ].data.length === 0 ) return;
 
-		if( this.zoom > 1 ) this.zoom = 1;
-		if( this.zoom < .1 ) this.zoom = .1;
+		//if( this.zoom > 1 ) this.zoom = 1;
+		//if( this.zoom < .1 ) this.zoom = .1;
 
-		var first = this.data[ 0 ].x;
-		var last = this.data[ this.data.length - 1 ].x;
+		var data = this.lines[ 0 ].data;
+
+		var first = data[ 0 ].x;
+		var last = data[ data.length - 1 ].x;
+		this.first = first;
+		this.last = last;
 		var w = last - first;
-		this.start = first + this.pivot * w - this.pivot * this.zoom * w;
-		this.end = first + ( this.pivot + ( 1 - this.pivot ) * this.zoom ) * w;
+		this.start = this.first;// - this.pivot * w / this.zoom;
+		this.end = this.last;// + ( 1 - this.pivot ) * w / this.zoom;
 
 		this.max = 0;
 		this.min = Number.MAX_VALUE;
 
-		this.data.forEach( ( v, i ) => {
+		data.forEach( ( v, i ) => {
 			if( v.x >= this.start && v.x <= this.end ) {
 				if( v.y < this.min ) this.min = v.y;
 				if( v.y > this.max ) this.max = v.y;
@@ -275,82 +367,55 @@
 
 	}
 
-	function createAdjustFunction( min, max, size ) {
-
-		return function adjust( v ) {
-
-			return ( v - min ) * size / ( max - min );
-
-		}
-
-	}
-
-	Graph.prototype.updatePath = function() {
-
-		this.path = new Path2D();
-
-		var ovx = 0;
-		var ovy = 0;
-		var start = this.data[ 0 ].x;
-
-		this.data.forEach( ( v, i ) => {
-
-			var vx = v.x - start;
-			var vy = v.y / this.max;
-
-			if( i === 0 ) {
-				this.path.moveTo( vx, vy );
-				ovx = vx;
-				ovy = vy;
-			} else {
-				var cpx = ovx + ( vx - ovx ) * .5;
-				var cpy1 = ( vy < ovy ) ? vy : ovy;
-				var cpy2 = ( vy < ovy ) ? ovy : vy;
-				//path.lineTo( this.adjustX( ~~v.x ), this.adjustY( v.y ) );
-				this.path.bezierCurveTo( cpx, cpy1, cpx, cpy2, vx, vy );
-				ovx = vx;
-				ovy = vy;
-			}
-
-		} );
-
-		this.path2 = new Path2D( this.path );
-		this.path2.lineTo( this.data[ this.data.length - 1 ].x - start, 0 );
-		this.path2.lineTo( 0, 0 );
-
-	}
-
 	Graph.prototype.draw = function() {
 
-		if( !this.data.length ) return;
-
 		this.ctx.save();
-
-		this.adjustX = createAdjustFunction( this.start, this.end, this.canvas.width );
-		this.adjustY = createAdjustFunction( this.max, 0, this.canvas.height - this.paddingTop );
 
 		this.ctx.fillStyle = '#efefef'
 		this.ctx.fillRect( 0, 0, this.canvas.width, this.canvas.height );
 
-		var zoom = this.zoom * ( this.end - this.start ) / this.canvas.width;
+		var p = this.pivot;
+		var w = this.end - this.start;
 		var h = ( this.canvas.clientHeight - this.paddingTop ) * this.dpr;
-		this.ctx.translate( this.hOffset, h + this.paddingTop * this.dpr );
-		this.ctx.scale( 1 / zoom, -h );
+		this.ctx.scale( this.canvas.width / w, 1 );
+		this.ctx.translate( 0, h + this.paddingTop * this.dpr );
+		//this.ctx.translate( this.start, 0 );
+		this.ctx.scale( this.zoom, -h / this.max );
+		//this.ctx.translate( p * -w, 0 );
 
-		this.ctx.lineWidth = 1 / h;
-		this.ctx.stroke( this.path );
+		var border = 1;
+		var step = border;
+		this.lines.forEach( line => {
 
-		this.ctx.fillStyle = this.properties.color;
-		this.ctx.fill( this.path2 );
+			if( this.lines.length == 1 ) {
 
-		/*this.ctx.translate( 0, 2 );
-		this.ctx.lineWidth = 1.5;
-		this.ctx.globalCompositeOperation = 'color-burn';
-		this.ctx.strokeStyle = '#000000'
-		this.ctx.globalAlpha = .1;
-		this.ctx.stroke( this.path );
+				this.ctx.fillStyle = line.secondaryColor;
+				this.ctx.fill( line.solidPath );
+				this.ctx.translate( - step, 0 );
+				this.ctx.fill( line.solidPath );
+				this.ctx.translate( 2 * step, 0 );
+				this.ctx.fill( line.solidPath );
+				this.ctx.translate( - step, 0 );
 
-		this.ctx.translate( 0, -2 );*/
+				this.ctx.translate( 0, - border / this.canvas.height );
+				this.ctx.fillStyle = line.color;
+				this.ctx.fill( line.solidPath );
+				this.ctx.translate( 0, border / this.canvas.height );
+
+			} else {
+
+				this.ctx.lineWidth = 1;
+				this.ctx.strokeStyle = line.color;
+				this.ctx.stroke( line.linePath );
+				this.ctx.translate( - step, 0 );
+				this.ctx.stroke( line.linePath );
+				this.ctx.translate( 2 * step, 0 );
+				this.ctx.stroke( line.linePath );
+				this.ctx.translate( - step, 0 );
+
+			}
+
+		} );
 
 		/*if( this.properties.baselines.length ) {
 
