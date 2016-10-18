9deffc4859f25e9b519d13d8b698ab73d5b70309
Jaume Sanchez <the.spite@gmail.com>
2016-10-18 02:28:15 +0200

    moved methods to wrapper

--- canvas-instrument.js
+++ canvas-instrument.js
@@ -44,20 +44,35 @@
 
 	}
 
-	function ContextData( context ) {
+	function ContextData( contextWrapper ) {
 
 		this.id = createUUID();
 		this.queryExt = null;
-		this.context = context;
+		this.contextWrapper = contextWrapper;
 
 	}
 
 	function WebGLRenderingContextWrapper( context ) {
 
+		this.id = createUUID();
 		this.context = context;
 
 		this.queryStack = [];
 		this.activeQuery = null;
+
+		this.count = 0;
+
+	}
+
+	WebGLRenderingContextWrapper.prototype.resetCount = function() {
+
+		this.count = 0;
+
+	}
+
+	WebGLRenderingContextWrapper.prototype.incrementCount = function() {
+
+		this.count++;
 
 	}
 
@@ -66,6 +81,7 @@
 		try{
 			if( typeof WebGLRenderingContext.prototype[ key ] === 'function' ) {
 				WebGLRenderingContextWrapper.prototype[ key ] = function() {
+					this.incrementCount();
 					return WebGLRenderingContext.prototype[ key ].apply( this.context, arguments );
 				}
 			} else {
@@ -77,10 +93,11 @@
 
 	} );
 
-	function WebGLDebugShadersExtensionWrapper( context ) {
+	function WebGLDebugShadersExtensionWrapper( contextWrapper ) {
 
-		this.context = context;
-		this.extension = WebGLRenderingContext.prototype.getExtension.apply( this.context, [ 'WEBGL_debug_shaders' ] );
+		this.id = createUUID();
+		this.contextWrapper = contextWrapper;
+		this.extension = WebGLRenderingContext.prototype.getExtension.apply( this.contextWrapper.context, [ 'WEBGL_debug_shaders' ] );
 
 	}
 
@@ -115,12 +132,12 @@
 		log( arguments );
 
 		var context = getContext.apply( this, arguments );
-		var cData = new ContextData( context );
-		contexts.push( cData );
 
 		if( arguments[ 0 ] === 'webgl' || arguments[ 0 ] === 'experimental-webgl' ) {
 			var wrapper = new WebGLRenderingContextWrapper( context );
+			var cData = new ContextData( wrapper );
 			cData.queryExt = wrapper.getExtension( 'EXT_disjoint_timer_query' )
+			contexts.push( cData );
 			return wrapper;
 		} else {
 			return context;
@@ -135,61 +152,6 @@
 	var getShaderInfoLog = WebGLRenderingContext.prototype.getShaderInfoLog;
 	var deleteShader = WebGLRenderingContext.prototype.deleteShader;
 
-	function WebGLShaderWrapper( context, type ) {
-
-		this.context = context;
-		this.shader = createShader.apply( this.context, [ type ] );
-		this.version = 1;
-		this.source = null;
-		this.type = type;
-
-	}
-
-	WebGLShaderWrapper.prototype.shaderSource = function( source ) {
-
-		this.source = source;
-		return shaderSource.apply( this.context, [ this.shader, source ] );
-
-	}
-
-	/*
-	WebGLRenderingContext.prototype.createShader = function() {
-
-		log( 'create shader' );
-		return new WebGLShaderWrapper( this, arguments[ 0 ] );
-
-	}
-
-	WebGLRenderingContext.prototype.shaderSource = function() {
-
-		return arguments[ 0 ].shaderSource( arguments[ 1 ] );
-
-	}
-
-	WebGLRenderingContext.prototype.compileShader = function() {
-
-		return compileShader.apply( this, [ arguments[ 0 ].shader ] );
-
-	}
-
-	WebGLRenderingContext.prototype.getShaderParameter = function() {
-
-		return getShaderParameter.apply( this, [ arguments[ 0 ].shader, arguments[ 1 ] ] );
-
-	}
-
-	WebGLRenderingContext.prototype.getShaderInfoLog = function() {
-
-		return getShaderInfoLog.apply( this, [ arguments[ 0 ].shader ] );
-
-	}
-
-	WebGLRenderingContext.prototype.deleteShader = function() {
-
-		return deleteShader.apply( this, [ arguments[ 0 ].shader ] );
-
-	}
-	*/
 	var createProgram = WebGLRenderingContext.prototype.createProgram;
 	var attachShader = WebGLRenderingContext.prototype.attachShader;
 	var detachShader = WebGLRenderingContext.prototype.detachShader;
@@ -203,10 +165,65 @@
 	var getUniformLocation = WebGLRenderingContext.prototype.getUniformLocation;
 	var useProgram = WebGLRenderingContext.prototype.useProgram;
 
-	function WebGLUniformLocationWrapper( context, program, name ) {
+	function WebGLShaderWrapper( contextWrapper, type ) {
 
 		this.id = createUUID();
-		this.context = context;
+		this.contextWrapper = contextWrapper;
+		this.shader = createShader.apply( this.contextWrapper.context, [ type ] );
+		this.version = 1;
+		this.source = null;
+		this.type = type;
+
+	}
+
+	WebGLShaderWrapper.prototype.shaderSource = function( source ) {
+
+		this.source = source;
+		return shaderSource.apply( this.contextWrapper.context, [ this.shader, source ] );
+
+	}
+
+	WebGLRenderingContextWrapper.prototype.createShader = function() {
+
+		log( 'create shader' );
+		return new WebGLShaderWrapper( this, arguments[ 0 ] );
+
+	}
+
+	WebGLRenderingContextWrapper.prototype.shaderSource = function() {
+
+		return arguments[ 0 ].shaderSource( arguments[ 1 ] );
+
+	}
+
+	WebGLRenderingContextWrapper.prototype.compileShader = function() {
+
+		return compileShader.apply( this.context, [ arguments[ 0 ].shader ] );
+
+	}
+
+	WebGLRenderingContextWrapper.prototype.getShaderParameter = function() {
+
+		return getShaderParameter.apply( this.context, [ arguments[ 0 ].shader, arguments[ 1 ] ] );
+
+	}
+
+	WebGLRenderingContextWrapper.prototype.getShaderInfoLog = function() {
+
+		return getShaderInfoLog.apply( this.context, [ arguments[ 0 ].shader ] );
+
+	}
+
+	WebGLRenderingContextWrapper.prototype.deleteShader = function() {
+
+		return deleteShader.apply( this.context, [ arguments[ 0 ].shader ] );
+
+	}
+
+	function WebGLUniformLocationWrapper( contextWrapper, program, name ) {
+
+		this.id = createUUID();
+		this.contextWrapper = contextWrapper;
 		this.program = program;
 		this.name = name;
 		this.getUniformLocation();
@@ -219,15 +236,15 @@
 
 	WebGLUniformLocationWrapper.prototype.getUniformLocation = function() {
 
-		this.uniformLocation = getUniformLocation.apply( this.context, [ this.program.program, this.name ] );
+		this.uniformLocation = getUniformLocation.apply( this.contextWrapper, [ this.program.program, this.name ] );
 
 	}
 
-	function WebGLProgramWrapper( context ) {
+	function WebGLProgramWrapper( contextWrapper ) {
 
 		this.id = createUUID();
-		this.context = context;
-		this.program = createProgram.apply( this.context );
+		this.contextWrapper = contextWrapper;
+		this.program = createProgram.apply( this.contextWrapper.context );
 		this.version = 1;
 		this.vertexShaderWrapper = null;
 		this.fragmentShaderWrapper = null;
@@ -240,102 +257,100 @@
 
 		var shaderWrapper = arguments[ 0 ];
 
-		if( shaderWrapper.type == this.context.VERTEX_SHADER ) this.vertexShaderWrapper = shaderWrapper;
-		if( shaderWrapper.type == this.context.FRAGMENT_SHADER ) this.fragmentShaderWrapper = shaderWrapper;
+		if( shaderWrapper.type == this.contextWrapper.context.VERTEX_SHADER ) this.vertexShaderWrapper = shaderWrapper;
+		if( shaderWrapper.type == this.contextWrapper.context.FRAGMENT_SHADER ) this.fragmentShaderWrapper = shaderWrapper;
 
-		return attachShader.apply( this.context, [ this.program, shaderWrapper.shader ] );
+		return attachShader.apply( this.contextWrapper.context, [ this.program, shaderWrapper.shader ] );
 
 	}
 
 	WebGLProgramWrapper.prototype.highlight = function() {
 
-		detachShader.apply( this.context, [ this.program, this.fragmentShaderWrapper.shader ] );
+		detachShader.apply( this.contextWrapper.context, [ this.program, this.fragmentShaderWrapper.shader ] );
 
 		var fs = this.fragmentShaderWrapper.source;
 		fs = fs.replace( /\s+main\s*\(/, ' ShaderEditorInternalMain(' );
 		fs += '\r\n' + 'void main() { ShaderEditorInternalMain(); gl_FragColor.rgb *= vec3(1.,0.,1.); }';
 
-		var highlightShaderWrapper = new WebGLShaderWrapper( this.context, this.context.FRAGMENT_SHADER );
+		var highlightShaderWrapper = new WebGLShaderWrapper( this.contextWrapper, this.contextWrapper.context.FRAGMENT_SHADER );
 		highlightShaderWrapper.shaderSource( fs );
-		compileShader.apply( this.context, [ highlightShaderWrapper.shader ] );
-		attachShader.apply( this.context, [ this.program, highlightShaderWrapper.shader ] );
-		linkProgram.apply( this.context, [ this.program ] );
+		compileShader.apply( this.contextWrapper.context, [ highlightShaderWrapper.shader ] );
+		attachShader.apply( this.contextWrapper.context, [ this.program, highlightShaderWrapper.shader ] );
+		linkProgram.apply( this.contextWrapper.context, [ this.program ] );
 
 		Object.keys( this.uniformLocations ).forEach( name => {
 			this.uniformLocations[ name ].getUniformLocation();
 		} );
 
 	}
-	/*
-	WebGLRenderingContext.prototype.createProgram = function() {
+
+	WebGLRenderingContextWrapper.prototype.createProgram = function() {
 
 		log( 'create program' );
 		return new WebGLProgramWrapper( this );
 
 	}
 
-	WebGLRenderingContext.prototype.attachShader = function() {
+	WebGLRenderingContextWrapper.prototype.attachShader = function() {
 
 		return arguments[ 0 ].attachShader( arguments[ 1 ] );
 
 	}
 
-	WebGLRenderingContext.prototype.linkProgram = function() {
+	WebGLRenderingContextWrapper.prototype.linkProgram = function() {
 
-		return linkProgram.apply( this, [ arguments[ 0 ].program ] );
-
-	}
-
-	WebGLRenderingContext.prototype.getProgramParameter = function() {
-
-		return getProgramParameter.apply( this, [ arguments[ 0 ].program, arguments[ 1 ] ] );
+		return linkProgram.apply( this.context, [ arguments[ 0 ].program ] );
 
 	}
 
-	WebGLRenderingContext.prototype.getProgramInfoLog = function() {
+	WebGLRenderingContextWrapper.prototype.getProgramParameter = function() {
 
-		return getProgramInfoLog.apply( this, [ arguments[ 0 ].program ] );
-
-	}
-
-	WebGLRenderingContext.prototype.getActiveAttrib = function() {
-
-		return getActiveAttrib.apply( this, [ arguments[ 0 ].program, arguments[ 1 ] ] );
+		return getProgramParameter.apply( this.context, [ arguments[ 0 ].program, arguments[ 1 ] ] );
 
 	}
 
-	WebGLRenderingContext.prototype.getAttribLocation = function() {
+	WebGLRenderingContextWrapper.prototype.getProgramInfoLog = function() {
 
-		return getAttribLocation.apply( this, [ arguments[ 0 ].program, arguments[ 1 ] ] );
-
-	}
-
-	WebGLRenderingContext.prototype.bindAttribLocation = function() {
-
-		return bindAttribLocation.apply( this, [ arguments[ 0 ].program, arguments[ 1 ], arguments[ 2 ] ] );
+		return getProgramInfoLog.apply( this.context, [ arguments[ 0 ].program ] );
 
 	}
 
-	WebGLRenderingContext.prototype.getActiveUniform = function() {
+	WebGLRenderingContextWrapper.prototype.getActiveAttrib = function() {
 
-		return getActiveUniform.apply( this, [ arguments[ 0 ].program, arguments[ 1 ] ] );
-
-	}
-
-	WebGLRenderingContext.prototype.getUniformLocation = function() {
-
-		return new WebGLUniformLocationWrapper( this, arguments[ 0 ], arguments[ 1 ] );
+		return getActiveAttrib.apply( this.context, [ arguments[ 0 ].program, arguments[ 1 ] ] );
 
 	}
 
-	WebGLRenderingContext.prototype.useProgram = function() {
+	WebGLRenderingContextWrapper.prototype.getAttribLocation = function() {
 
-		return useProgram.apply( this, [ arguments[ 0 ].program ] );
+		return getAttribLocation.apply( this.context, [ arguments[ 0 ].program, arguments[ 1 ] ] );
 
 	}
-	*/
 
-	/*
+	WebGLRenderingContextWrapper.prototype.bindAttribLocation = function() {
+
+		return bindAttribLocation.apply( this.context, [ arguments[ 0 ].program, arguments[ 1 ], arguments[ 2 ] ] );
+
+	}
+
+	WebGLRenderingContextWrapper.prototype.getActiveUniform = function() {
+
+		return getActiveUniform.apply( this.context, [ arguments[ 0 ].program, arguments[ 1 ] ] );
+
+	}
+
+	WebGLRenderingContextWrapper.prototype.getUniformLocation = function() {
+
+		return new WebGLUniformLocationWrapper( this.context, arguments[ 0 ], arguments[ 1 ] );
+
+	}
+
+	WebGLRenderingContextWrapper.prototype.useProgram = function() {
+
+		return useProgram.apply( this.context, [ arguments[ 0 ].program ] );
+
+	}
+
 	var methods = [
 		'uniform1f', 'uniform1fv', 'uniform1i', 'uniform1iv',
 		'uniform2f', 'uniform2fv', 'uniform2i', 'uniform2iv',
@@ -349,14 +364,13 @@
 	methods.forEach( method => {
 		var original = WebGLRenderingContext.prototype[ method ];
 		originalMethods[ method ] = original;
-		WebGLRenderingContext.prototype[ method ] = function() {
+		WebGLRenderingContextWrapper.prototype[ method ] = function() {
 			var args = [].slice.call( arguments );
 			if( !args[ 0 ] ) return;
 			args[ 0 ] = args[ 0 ].uniformLocation;
-			return original.apply( this, args );
+			return original.apply( this.context, args );
 		}
 	} );
-	*/
 
 	function WebGLTimerQueryEXTWrapper( contextWrapper, extension ) {
 
@@ -481,12 +495,17 @@
 
 	function processRequestAnimationFrames() {
 
-		var ext = contexts[ 0 ] ? contexts[ 0 ].queryExt : null;
-		if( ext ) {
-			var query = ext.createQueryEXT();
-			ext.beginQueryEXT( ext.TIME_ELAPSED_EXT, query );
-			extQueries.push( query );
-		}
+		contexts.forEach( ctx => {
+
+			ctx.contextWrapper.resetCount();
+
+			var ext = ctx.queryExt;
+			if( ext ) {
+				var query = ext.createQueryEXT();
+				ext.beginQueryEXT( ext.TIME_ELAPSED_EXT, query );
+				extQueries.push( query );
+			}
+		} );
 
 		var queue = rAFQueue.slice( 0 );
 		rAFQueue.length = 0;
@@ -494,23 +513,34 @@
 			rAF();
 		} );
 
-		if( ext ) {
+		contexts.forEach( ctx => {
 
-			ext.endQueryEXT( ext.TIME_ELAPSED_EXT );
+			var ext = ctx.queryExt;
+			var time = 0;
 
-			extQueries.forEach( ( query, i ) => {
+			if( ext ) {
 
-				var available = ext.getQueryObjectEXT( query, ext.QUERY_RESULT_AVAILABLE_EXT );
-				var disjoint = renderer.context.getParameter( ext.GPU_DISJOINT_EXT );
+				ext.endQueryEXT( ext.TIME_ELAPSED_EXT );
 
-				if (available && !disjoint) {
-					var time = ext.getQueryObjectEXT( query, ext.QUERY_RESULT_EXT );
-					console.log( time );
-					extQueries.splice( i, 1 );
-				}
+				extQueries.forEach( ( query, i ) => {
 
-			});
-		}
+					var available = ext.getQueryObjectEXT( query, ext.QUERY_RESULT_AVAILABLE_EXT );
+					var disjoint = ctx.contextWrapper.context.getParameter( ext.GPU_DISJOINT_EXT );
+
+					if (available && !disjoint) {
+						var queryTime = ext.getQueryObjectEXT( query, ext.QUERY_RESULT_EXT );
+						time += queryTime;
+						extQueries.splice( i, 1 );
+					}
+
+				});
+			}
+
+			if( ctx.contextWrapper.count ) {
+				console.log( ctx.contextWrapper.id, ctx.contextWrapper.count, time );
+			}
+
+		} );
 
 		originalRequestAnimationFrame( processRequestAnimationFrames );
 
