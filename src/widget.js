a4ebc12daf42bdf99aece2a68bfe3775ce2b5bcf
Jaume Sanchez <the.spite@gmail.com>
2016-10-25 03:20:02 +0200

    added formatNumber

--- src/widget.js
+++ src/widget.js
@@ -22,6 +22,22 @@ if( !window.document.body ) {
 	setupUI();
 }
 
+function formatNumber( value, sizes, decimals ) {
+
+	if(value == 0) return '0';
+
+	var k = 1000; // or 1024 for binary
+	var dm = decimals || 2;
+	var i = Math.floor(Math.log(value) / Math.log(k));
+
+	return parseFloat((value / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+
+}
+
+var timeSizes = ['ns', 'µs', 'ms', 's' ];
+var callSizes = [ '', 'K', 'M', 'G' ];
+var memorySizes = [ 'Bytes', 'KB', 'MB', 'GB', 'TB' ];
+
 function updateUI( e ) {
 
 	const d = e.detail;
